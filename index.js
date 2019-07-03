e29033b065d6db9ceb1d91b0fbf2e808724155e9
Nicholas Wright <nick@nicholaswright.co.uk>
2019-07-03 10:54:40 +0100

    Update index.js
    Items with tracking disabled (stockQuantity = null) will not show out of stock.
    Some settings can now be overridden by data parameters.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
     $.productStockControl = function(options) {
         
         var settings = $.extend({}, {
+            tableName: 'm_products_products',
             attr: 'data-product-stock-control',
             quantityAttr: 'data-product-stock-control--quantity',
             quantityHiddenClassName: 'hidden',
@@ -18,10 +19,9 @@
             return;
         }
         
-        var tableName;
         var productIds = [];
         
-        setTableName();
+        setParameterSettings();
         setProductIds();
         disableElements();
         updateQuantities();
@@ -29,12 +29,20 @@
             updateQuantities();
         }, 3000);
         
-        function setTableName() {
+        function setParameterSettings() {
+            var params = {
+                tableName: 'data-table-name',
+                singularLabel: 'data-singular-label',
+                pluralLabel: 'data-plural-label'
+            };
             if (els.length) {
-                tableName = els[0].getAttribute('data-table-name');
-                if (!tableName) {
-                    tableName = 'm_products_products';
-                }
+                var el = els[0];
+                Object.keys(params).map(function(settingName, index) {
+                    var attrName = params[settingName];
+                    if (el.hasAttribute(attrName)) {
+                        settings[settingName] = el.getAttribute(attrName);
+                    }
+                });
             }
         }
         
@@ -52,16 +60,21 @@
         
         function updateQuantities() {
             var url = '/controller/product/quantity?' + $.param({
-                tableName: tableName,
+                tableName: settings.tableName,
                 productIds: productIds
             });
             $.get(url, function(items) {
                 els.forEach(function(el) {
                     var productId = parseInt(el.getAttribute(settings.attr));
                     items.forEach(function(obj) {
-                        if (obj.id === productId) {
+                        if (
+                            obj.id === productId
+                            // The quantity is null when stock tracking is disabled.
+                            && obj.stockQuantity !== null
+                        ) {
                             var msg = obj.stockQuantity ? obj.stockQuantity + ' ' + (obj.stockQuantity === 1 ? settings.singularLabel : settings.pluralLabel) : settings.outOfStockMessage;
                             $(el).find('[' + settings.quantityAttr + ']').removeClass(settings.quantityHiddenClassName).html(msg);
+                            
                             if (obj.stockQuantity) {
                                 $(el).find('[' + settings.disabledAttr + ']').attr('disabled', false);
                                 $(el).find('[' + settings.hiddenAttr + ']').css('display', 'inline');
